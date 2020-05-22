10a0a2fed36669b466d36c5a55300eb3d30527d5
julie <julie@julies-MacBook-Pro.local>
2020-05-21 21:56:23 -0700

    update settings

--- recipes/settings.py
+++ recipes/settings.py
@@ -83,12 +83,12 @@ DATABASES = {
     }
 }
 
-# # dj-database-url settings
-# db_from_env = dj_database_url.config()
-# DATABASES['default'].update(db_from_env)
-# # Update database configuration with $DATABASE_URL.
-# db_from_env = dj_database_url.config()
-# DATABASES['default'].update(db_from_env)
+# dj-database-url settings
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
